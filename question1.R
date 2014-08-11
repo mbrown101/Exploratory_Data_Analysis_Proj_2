f3339e3490079bd6a6756695f8bce774767765a1
mbrown101 <m.g.brown85@gmail.com>
2014-08-11 16:14:29 -0700

    Create question1.R

---
+++ question1.R
@@ -0,0 +1,28 @@
+
+#   Project : Exploratory Data Analysis | Course Project 2
+#   Question 1: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+#   Author: M. Brown
+
+############################################################################################################
+
+
+# define working directory, create if it does not exist and set working directory
+mainDir <- "C:/Users/Mike/Documents/R/Exploratory_Data_Analysis"
+if (!file.exists(mainDir)){
+  dir.create(file.path(mainDir))
+}
+
+setwd("C:/Users/Mike/Documents/R/Exploratory_Data_Analysis")
+
+# get PM25_emissions_data
+data_file_path = paste(getwd() , "/PM25_emissions_data.zip" , sep = '')
+
+download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip' , data_file_path)
+unzip(data_file_path)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+head(NEI)
+head(SCC)
+  

fcff99555a9eb75d687e117e2d401f9ed03c7dee
mbrown101 <m.g.brown85@gmail.com>
2014-08-15 21:28:25 -0700

    Update question1.R

--- question1.R
+++ question1.R
@@ -23,7 +23,11 @@ unzip(data_file_path)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-head(NEI)
-head(SCC)
-  
 NEI_agg <- aggregate( NEI[,4] , by = list(NEI[,6]) , FUN = sum , na.rm = TRUE )
+
+par(mar=c(4, 5, 4, 0.5))
+q1plot <-barplot(NEI_agg[,2] , main = 'Total PM2.5 Emissions Decrease from 1999 to 2008' , ylab = 'PM2.5 emitted [tons]' , names = NEI_agg[,1] , col = 'red')
+print (q1plot)
+
+dev.copy(device=png,"q1plot.png" , width = 600, height = 480)
+dev.off()

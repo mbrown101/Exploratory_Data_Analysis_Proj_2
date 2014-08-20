3945d10b343b4dbee6942c4728d511208cc7622f
mbrown101 <m.g.brown85@gmail.com>
2014-08-20 16:40:27 -0700

    Update q4plot.R

--- q4plot.R
+++ q4plot.R
@@ -1,4 +1,5 @@
 
+
 #   Project : Exploratory Data Analysis | Course Project 2
 #   Question 4: Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
 #   Author: M. Brown
@@ -30,24 +31,21 @@ SCC.coal.df <- SCC[grepl('Coal' , SCC[,3]),]
 # merge the SCC coal entries with NEI 
 data.merge.coal <- merge(NEI , SCC.coal.df , by.x = 'SCC' , by.y = 'SCC')
 
-# Subset for only FIPS code = 24510 (Baltimore, MD)
-NEI.24510 <- subset(NEI , NEI[,1] == 24510)
-
 #aggregate total emissions by year
-NEI_agg <- aggregate( NEI.24510[,4] , by = list(NEI.24510[,6] , NEI.24510[,5]) , FUN = sum , na.rm = TRUE )
+coal.agg <- aggregate( data.merge.coal[,4] , by = list(data.merge.coal[,6] , data.merge.coal[,5]) , FUN = sum , na.rm = TRUE )
 
 #name columns after aggregation 
-colnames(NEI_agg) <- c('year' , 'type' , 'Emissions')
+colnames(coal.agg) <- c('year' , 'type' , 'Emissions')
 
 #change year data to character so GGplot doesn't scale the integers on the x-axis
-NEI_agg[,1] <- as.character(NEI_agg[,1])
+coal.agg[,1] <- as.character(coal.agg[,1])
 
 #format plot
 par(mar=c(4, 5, 4, 0.5))
 library(ggplot2)
-q4plot <- ggplot(data = NEI_agg , aes(x = year , y = Emissions , fill = type)) + 
+q4plot <- ggplot(data = coal.agg , aes(x = year , y = Emissions , fill = type)) + 
   geom_bar(stat = 'identity' , position = position_dodge() , color = 'black') +  
-  ggtitle("Emissions by Type for Baltimore. MD\nfor Selected Years 1999 - 2008") + 
+  ggtitle("PM2.5 Coal Emissions for Selected Years 1999 - 2008") + 
   theme(plot.title = element_text(lineheight=.8, face="bold"))
 
 
@@ -57,5 +55,3 @@ print (q4plot)
 #create .PNG file of plot in working directory 
 dev.copy(device=png,"q4plot.png" , width = 600, height = 480)
 dev.off()
-
-NEI_agg.1 <- subset(NEI_agg , year == '1999' | year == '2008')
